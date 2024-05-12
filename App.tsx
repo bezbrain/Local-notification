2f3dbda049906e47f08d7d9a8f5903decce170c4
bezbrain <belloquadriolawale@gmail.com>
2024-05-12 19:14:41 +0100

    Set up notification setting

--- App.tsx
+++ App.tsx
@@ -1,10 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+// INITIATE THE TRIGGER
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the body of the notification",
+        data: { testing: "Status" },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
+
+  useEffect(() => {
+    // This notification function will run whenever a new notification is received
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification.request.content.data);
+      }
+    );
+
+    // This runs when the notification is tapped
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+      }
+    );
+
+    return () => {
+      // These will be called whenever this component is removed from the screen. It is advisable to always call this for clean up
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Notification" onPress={scheduleNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +64,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
